04fc50142fd8c2967e8e64cee3062b84d887d8f1
Gnarnold <nickrnld56@gmail.com>
2019-03-26 00:04:26 -0500

    Updates

---
+++ precache-manifest.a68f3e51afbeefc7501eb56ccf0985d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe81c4c31232682d87b",
+    "url": "/clickyGame/static/js/runtime~main.62255258.js"
+  },
+  {
+    "revision": "17e4cec3a0f16304ac80",
+    "url": "/clickyGame/static/js/main.546b4e48.chunk.js"
+  },
+  {
+    "revision": "69eb99e78a679b2f0550",
+    "url": "/clickyGame/static/js/2.a9af9fca.chunk.js"
+  },
+  {
+    "revision": "17e4cec3a0f16304ac80",
+    "url": "/clickyGame/static/css/main.790517e6.chunk.css"
+  },
+  {
+    "revision": "f08d7c19bc2e2c0b0cd8fda5cc2fb1be",
+    "url": "/clickyGame/index.html"
+  }
+];
